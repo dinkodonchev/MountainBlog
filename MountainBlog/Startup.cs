5922ed276de6b596ab1deec2b154afa0e1200872
dinkodonchev <dinyo.donchev@abv.bg>
2017-03-01 12:51:15 +0200

    initialize

---
+++ MountainBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MountainBlog.Startup))]
+namespace MountainBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
